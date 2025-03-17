dbb4c2a7114c1709c0238d4f44e1d2b7be86f395
akshu0207 <163676215+akshu0207@users.noreply.github.com>
2025-03-17 18:00:49 +0530

    batch file is added

---
+++ build_features.bat
@@ -0,0 +1,23 @@
+@echo off
+cd C:\Users\Akshaya\Documents\Appeon\PowerBuilder 22.0\github\features
+
+echo pulling the code from github...
+git pull origin features
+
+if %errorlevel% neq 0 (
+     echo pull got failed!
+     pause
+)
+echo Building power builder application...
+"C:\Users\Akshaya\Documents\Appeon\PBTools\PBAutobuild220.exe" /f "C:\Users\Akshaya\Documents\Appeon\PowerBuilder 22.0\github\features\genapp.json"
+if %errorlevel% neq 0 (
+     echo power bulider Build failed!
+     pause
+)
+echo Build Successful. moving app.exe to deployment folder Deploying...
+Xcopy "C:\Users\Akshaya\Documents\Appeon\PowerBuilder 22.0\github\features\genapp.pbd" "C:\Users\Akshaya\Documents\Appeon\PowerBuilder 22.0\DeployedApp_features\" /Y
+echo Deployment complete!
+if %errorlevel% neq 0 (
+      echo power builder deployment failed!
+      pause
+)  
