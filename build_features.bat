ada6b4b71322c8b9cba3f2bd5ccb49031e04e146
akshu0207 <163676215+akshu0207@users.noreply.github.com>
2025-04-16 17:03:50 +0530

    Update build_features.bat

--- build_features.bat
+++ build_features.bat
@@ -1,5 +1,5 @@
 @echo off
-cd C:\Users\Akshaya\Documents\Appeon\PowerBuilder 22.0\github\features
+cd C:\Users\Akshaya\Documents\Appeon\PowerBuilder 22.0\github\AscentHR
 
 echo pulling the code from github...
 git pull origin features
@@ -9,13 +9,13 @@ if %errorlevel% neq 0 (
      pause
 )
 echo Building power builder application...
-"C:\Users\Akshaya\Documents\Appeon\PBTools\PBAutobuild220.exe" /f "C:\Users\Akshaya\Documents\Appeon\PowerBuilder 22.0\github\features\genapp.json"
+"C:\Users\Akshaya\Documents\Appeon\PBTools\PBAutobuild220.exe" /f "C:\Users\Akshaya\Documents\Appeon\PowerBuilder 22.0\github\AscentHR\genapp.json"
 if %errorlevel% neq 0 (
      echo power bulider Build failed!
      pause
 )
 echo Build Successful. moving app.exe to deployment folder Deploying...
-Xcopy "C:\Users\Akshaya\Documents\Appeon\PowerBuilder 22.0\github\features\genapp.pbd" "C:\Users\Akshaya\Documents\Appeon\PowerBuilder 22.0\DeployedApp_features\" /Y
+Xcopy "C:\Users\Akshaya\Documents\Appeon\PowerBuilder 22.0\github\AscentHR\genapp.pbd" "C:\Users\Akshaya\Documents\Appeon\PowerBuilder 22.0\DeployedApp_features\" /Y
 echo Deployment complete!
 if %errorlevel% neq 0 (
       echo power builder deployment failed!
